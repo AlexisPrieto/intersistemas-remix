57cba45cc953d80d109296b9137e059568ac1b80
Alexis Prieto <prietoalexis99@gmail.com>
2022-11-29 10:13:07 -0300

    avances noviembre

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,12 +1,23 @@
+import {Link} from '@remix-run/react';
+import Nosotros from './nosotros';
+import Amedida from './amedida';
+import Servicios from './servicios';
+import Contacto from './contacto';
 
 function Index() {
   return (
+      <div className=''>
+          <div className="index">
+            
+              <h1>Soluciones informáticas</h1>
+              somos parte de tu crecimiento
+          </div>
 
-    <div className="index">
-      
-      <h1>Soluciones informáticas</h1>
-        somos parte de tu crecimiento
-    </div>
+          <Nosotros/>
+          <Servicios/>
+          <Amedida/>
+          <Contacto/>
+      </div>
 
     
 
